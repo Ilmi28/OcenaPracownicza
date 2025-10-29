4c979f0dd46cc3808bf7abcfcfa535ed411da80f
Ilmi28 <ilmialiev28@gmail.com>
2025-10-29 18:42:49 +0100

    Add project files.

---
+++ src/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OcenaPracownicza.Controllers
+{
+    [ApiController]
+    [Route("test")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok("test");
+        }
+    }
+}
