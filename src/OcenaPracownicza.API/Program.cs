777e47d242343a4b30451a81f513377414b1055a
Ilmi28 <ilmialiev28@gmail.com>
2025-10-29 19:49:34 +0100

    asdf

--- src/OcenaPracownicza.API/Program.cs
+++ src/OcenaPracownicza.API/Program.cs
@@ -16,7 +16,7 @@ namespace OcenaPracownicza
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+            // Configure the HTTP request pipeline.sdfsdf
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
