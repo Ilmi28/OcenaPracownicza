d7852b4eb531f5be210d6417381415fdad7044ce
Ilmi28 <ilmialiev28@gmail.com>
2025-11-07 22:22:01 +0100

    logowanie poprzez ciasteczka oraz dodanie przykladowych rzeczy

--- src/ocena-pracownicza-app/vite.config.ts
+++ src/ocena-pracownicza-app/vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    server: {
+        host: 'localhost',  
+        port: 5173,             
+        open: true         
+    }
 })
