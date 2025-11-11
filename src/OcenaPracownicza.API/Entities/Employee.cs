a8b33803e28143942ad0e447633028e0c60c3f08
Ilmi28 <ilmialiev28@gmail.com>
2025-11-11 23:46:13 +0100

    poprawki

---
+++ src/OcenaPracownicza.API/Entities/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace OcenaPracownicza.API.Entities
+{
+    public class Employee : BaseEntity
+    {
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string Position { get; set; }
+        public required string Department { get; set; }
+    }
+}
---
+++ src/OcenaPracownicza.API/Interfaces/Services/IDocumentGeneratorService.cs
@@ -0,0 +1,9 @@
+﻿using OcenaPracownicza.API.Entities;
+
+namespace Ocenapracownicza.API.Services
+{
+    public interface IDocumentGeneratorService
+    {
+        byte[] GenerateReport(Employee employee);
+    }
+}
