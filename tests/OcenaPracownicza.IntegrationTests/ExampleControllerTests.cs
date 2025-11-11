facbc0abd0432ab7c479779601edbc135c4a4061
PiotrCeberek <piotrceb1@gmail.com>
2025-11-12 00:13:19 +0100

    Wstępna konfiguracja i testy jednostkowe oraz integracyjne

---
+++ tests/OcenaPracownicza.IntegrationTests/ExampleControllerTests.cs
@@ -0,0 +1,76 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using OcenaPracownicza.API.Requests;
+using Xunit;
+
+namespace OcenaPracownicza.IntegrationTests
+{
+    public class ExampleControllerTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly HttpClient _client;
+
+        public ExampleControllerTests(WebApplicationFactory<Program> factory)
+        {
+            _client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task GetAll_ShouldReturnOk()
+        {
+            var response = await _client.GetAsync("/example");
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task GetById_ShouldReturnOk()
+        {
+            var response = await _client.GetAsync("/example/1");
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task Post_ShouldReturnOk_AndCreateItem()
+        {
+            var newItem = new ExampleRequest
+            {
+                Name = "Test Example",
+                Description = "Element testowy",
+                SomeDetail = "Szczegóły testowe"
+
+            };
+
+            var response = await _client.PostAsJsonAsync("/example", newItem);
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            var content = await response.Content.ReadAsStringAsync();
+            content.Should().Contain("Test Example");
+        }
+
+        [Fact]
+        public async Task Put_ShouldReturnOk_AndUpdateItem()
+        {
+            var updateItem = new ExampleRequest
+            {
+                Name = "Zaktualizowany Example",
+                Description = "Nowy opis",
+                SomeDetail = "Szczegóły testowe"
+            };
+
+            var response = await _client.PutAsJsonAsync("/example/1", updateItem);
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            var content = await response.Content.ReadAsStringAsync();
+            content.Should().Contain("Zaktualizowany");
+        }
+
+        [Fact]
+        public async Task Delete_ShouldReturnOk()
+        {
+            var response = await _client.DeleteAsync("/example/1");
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+    }
+}
---
+++ tests/OcenaPracownicza.UnitTests/DocumentGeneratorServiceTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using Xunit;
+using Ocenapracownicza.API.Services;
+
+namespace Ocenapracownicza.UnitTests
+{
+    public class DocumentGeneratorServiceTests
+    {
+        [Fact]
+        public void GenerateReport_ShouldReturnPdfBytes()
+        {
+            var service = new DocumentGeneratorService();
+            string employeeName = "Jan Kowalski";
+            string position = "Programista";
+            string period = "01.2025 - 06.2025";
+            string finalScore = "Dobry";
+            string achievementsSummary = "Test test.";
+
+            byte[] pdfBytes = service.GenerateReport(employeeName, position, period, finalScore, achievementsSummary);
+
+            Assert.NotNull(pdfBytes);
+            Assert.NotEmpty(pdfBytes);
+            Assert.True(pdfBytes.Length > 1000);
+        }
+    }
+}
