954015340d21a0d48dc503d735fffad5951ef2ba
Ilmi Aliev <86533605+Ilmi28@users.noreply.github.com>
2025-11-11 23:50:45 +0100

    poprawki (#9)
    * poprawki
    
    * poprawki

---
+++ src/OcenaPracownicza.API/Entities/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace OcenaPracownicza.API.Entities
+{
+    public class Employee : BaseEntity
+    {
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string Position { get; set; }
+        public required string Period { get; set; }
+        public required string FinalScore { get; set; }
+        public required string AchievementsSummary { get; set; }
+    }
+}
