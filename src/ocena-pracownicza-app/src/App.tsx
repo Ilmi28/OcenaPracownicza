5c5a41c3df5b551a2e12fdaab86acef361e4872c
Ilmi Aliev <86533605+Ilmi28@users.noreply.github.com>
2025-11-18 19:38:35 +0100

    Fix import statement for Button component

--- src/ocena-pracownicza-app/src/App.tsx
+++ src/ocena-pracownicza-app/src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Button from "@mui/material/Button";
+import Btton from "@mui/material/Button";
 import { authService } from "./services/authService";
 
 import axiosClient from "./services/axiosClient";
