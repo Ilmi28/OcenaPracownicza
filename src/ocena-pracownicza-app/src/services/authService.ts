9a0d235c777c5522512791fded6c641360b4fb32
Ilmi28 <ilmialiev28@gmail.com>
2025-11-18 18:45:30 +0100

    poprawki

--- src/ocena-pracownicza-app/src/services/authService.ts
+++ src/ocena-pracownicza-app/src/services/authService.ts
@@ -1,16 +1,16 @@
 import axiosClient from "./axiosClient";
 
 export const authService = {
-  login: async (username: string, password: string) => {
-    const res = await axiosClient.post("/auth/login", {
-      username,
-      password
-    });
-    return res.data;
-  },
+    login: async (username: string, password: string) => {
+        const res = await axiosClient.post("/auth/login", {
+            username,
+            password,
+        });
+        return res.data;
+    },
 
-  check: async () => {
-    const res = await axiosClient.get("/auth/secure");
-    return res.data;
-  },
+    check: async () => {
+        const res = await axiosClient.get("/auth/secure");
+        return res.data;
+    },
 };
--- src/ocena-pracownicza-app/src/services/axiosClient.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-const axiosClient = axios.create({
-    baseURL: "https://localhost:56517/api",
-    withCredentials: true,
-    headers: {
-        "Content-Type": "application/json"
-    }
-});
-export default axiosClient;
