b3e6fde021708ee540ed0a16c9672a585667d607
MarcinZalewski1 <89201@student.pb.edu.pl>
2026-04-13 20:27:05 +0200

    dodanie serwisu do generowania arkuszy oceny cyklicznie

---
+++ src/OcenaPracownicza.API/Services/EvaluationPeriodBackgroundService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Hosting;
+using OcenaPracownicza.API.Interfaces.Services;
+
+namespace OcenaPracownicza.API.Services;
+
+public class EvaluationPeriodBackgroundService : BackgroundService
+{
+    private readonly IServiceProvider _serviceProvider;
+    private readonly ILogger<EvaluationPeriodBackgroundService> _logger;
+
+    public EvaluationPeriodBackgroundService(
+        IServiceProvider serviceProvider, 
+        ILogger<EvaluationPeriodBackgroundService> logger)
+    {
+        _serviceProvider = serviceProvider;
+        _logger = logger;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        _logger.LogInformation("Uruchomiono usługę w tle do sprawdzania okresów oceny.");
+
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            try 
+            {
+                using (var scope = _serviceProvider.CreateScope())
+                {
+                    var generatorService = scope.ServiceProvider.GetRequiredService<IEvaluationSheetGeneratorService>();
+                    await generatorService.GenerateSheetsForCurrentPeriodAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Wystąpił błąd podczas automatycznego generowania arkuszy oceny.");
+            }
+
+            await Task.Delay(TimeSpan.FromDays(1), stoppingToken);
+        }
+    }
+}
